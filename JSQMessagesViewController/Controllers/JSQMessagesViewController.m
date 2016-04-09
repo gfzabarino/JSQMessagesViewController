a73402e14b26b6d09703dbb8fc0b3c92407e2ee3
Gian Franco Zabarino <gfzabarino@gmail.com>
2016-04-09 00:04:18 -0300

    Restored lost commit bb881715b7fe62f6638bc8c0f0b1fdb4fad033d9:
    - Added functionality to set an initial text when the view controller still didn't appear and have the text view resized correctly.

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -308,6 +308,21 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
     [self finishSendingMessageAnimated:YES];
 }
 
+- (void)configureWithInitialTextAndResize:(NSString *)initialText
+{
+    JSQMessagesComposerTextView *textView = self.inputToolbar.contentView.textView;
+    CGSize oldContentSize = textView.contentSize;
+    textView.text = initialText;
+    [self.inputToolbar setNeedsLayout];
+    [self.inputToolbar layoutIfNeeded];
+    [self.inputToolbar toggleSendButtonEnabled];
+    CGSize newContentSize = textView.contentSize;
+    [UIView performWithoutAnimation:^{
+        [self jsq_adjustWithOldContentSize:oldContentSize newContentSize:newContentSize];
+        [textView scrollRangeToVisible:NSMakeRange(textView.text.length - 1, 1)];
+    }];
+}
+
 - (void)finishSendingMessageAnimated:(BOOL)animated {
 
     UITextView *textView = self.inputToolbar.contentView.textView;
@@ -777,13 +792,7 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
             CGSize oldContentSize = [[change objectForKey:NSKeyValueChangeOldKey] CGSizeValue];
             CGSize newContentSize = [[change objectForKey:NSKeyValueChangeNewKey] CGSizeValue];
 
-            CGFloat dy = newContentSize.height - oldContentSize.height;
-
-            [self jsq_adjustInputToolbarForComposerTextViewContentSizeChange:dy];
-            [self jsq_updateCollectionViewInsets];
-            if (self.automaticallyScrollsToMostRecentMessage) {
-                [self scrollToBottomAnimated:NO];
-            }
+            [self jsq_adjustWithOldContentSize:oldContentSize newContentSize:newContentSize];
         }
     }
 }
@@ -939,6 +948,16 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
 }
 
 #pragma mark - Collection view utilities
+
+- (void)jsq_adjustWithOldContentSize:(CGSize)oldContentSize newContentSize:(CGSize)newContentSize {
+    CGFloat dy = newContentSize.height - oldContentSize.height;
+
+    [self jsq_adjustInputToolbarForComposerTextViewContentSizeChange:dy];
+    [self jsq_updateCollectionViewInsets];
+    if (self.automaticallyScrollsToMostRecentMessage) {
+        [self scrollToBottomAnimated:NO];
+    }
+}
 
 - (void)jsq_updateCollectionViewInsets
 {
